8d5f5c2d4e280f4eb30e2618455b74419e1ece5a
Natcha <natcha@Natcha.connectify>
2014-11-21 23:11:24 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.locals import *
+from memory import SimpleGame
+
+class Memory(SimpleGame):
+	pass
+
+def main():
+	game = Memory()
+	game.run()
+
+if __name__ == '__main__':
+	main()
+
+
+
