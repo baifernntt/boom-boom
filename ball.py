15768514caac020fc628142477100f526caef5cf
Natcha <natcha@46-234.kuwin.ku.ac.th>
2014-11-26 21:40:37 +0700

    add sound

--- ball.py
+++ ball.py
@@ -3,17 +3,17 @@ from pygame.locals import *
 
 class Ball(object):
 	up_lane = 139
-	down_lane = 239
-	def __init__(self,lane, x):
+#	down_lane = 239
+	def __init__(self, x):
 		self.x = x
-		self.y = 0
-		self.color = pygame.Color('Blue')
-		if lane==1:
-			self.color = pygame.Color('red')
-			self.y = Ball.up_lane
-		elif lane==2:
-			self.color = pygame.Color('blue')
-			self.y = Ball.down_lane	
+		self.y = Ball.up_lane
+		self.color = pygame.Color('red')
+		#if lane==1:
+			#self.color = pygame.Color('red')
+			#self.y = Ball.up_lane
+		#elif lane==2:
+			#self.color = pygame.Color('blue')
+			#self.y = Ball.down_lane	
 		self.radius = 28
 
 	def render(self,surface):
@@ -23,8 +23,37 @@ class Ball(object):
 		pass
 
 	def delete(self):
-		self.x = 1000
+		self.x = -1000
 
 	def update(self):
 		self.x -= 5
-		
+
+	def isCollide(self):
+		if (self.x <=100  and self.x+self.radius*2 >=100) or (self.x <=47  and self.x+self.radius*2 >=47):
+			#if pygame.key.get_pressed()[K_r]:
+				
+			return True
+
+class Blueball(object):
+	
+	down_lane = 239
+
+	def __init__(self, x):
+
+		self.x = x
+		self.y = Blueball.down_lane
+		self.color = pygame.Color('blue')
+		self.radius = 28
+
+	def render(self,surface):
+		self.circle = pygame.draw.circle(surface,self.color,(self.x,self.y), self.radius,0)
+
+	def delete(self):
+		self.x = -1000
+
+	def update(self):
+		self.x -= 7
+
+	def isCollide(self):
+		if (self.x <=100  and self.x+self.radius*2 >=100) or (self.x <=47  and self.x+self.radius*2 >=47):
+			return True	
--- main.py
+++ main.py
@@ -1,34 +1,56 @@
-import pygame
+import pygame, pygame.mixer
 import memory
 from pygame.locals import *
-from ball import Ball
+from ball import Ball,Blueball
+#from score import score
 
 class Memory(memory.SimpleGame):
-
 	BLACK = pygame.Color('black')
 	WHITE = pygame.Color('white')
 	RED = pygame.Color('red')
 	BLUE = pygame.Color('blue')
 	bg = pygame.image.load('bg.jpg')
+	pygame.mixer.pre_init(48000, -16, 2, 4096)
 
 	def init(self):
 	 	super(Memory,self).init()
+		global sound
+		sound = pygame.mixer.Sound('pon.wav')
 
 	def __init__(self):
-		global note
+		global note,note1
+#		score = score()
 	 	super(Memory,self).__init__('Boom boom',Memory.bg)
-		note = [Ball(1,260),Ball(2,254),Ball(1,130),Ball(2,480),Ball(1,480),Ball(2,480)]
+		note = [Ball(480),Ball(300),Ball(410),Ball(480)]
+		note1 = [Blueball(470),Blueball(330),Blueball(400),Blueball(450)]
 	#	self.redball = Ball(radius=28, pos=(480,139) ,color=Memory.RED)
 	#	self.blueball = Ball(radius=28,pos=(480,239),color=Memory.BLUE)
 
 	def render(self,surface):
+		pygame.draw.circle(surface, pygame.Color('black'),(100,139),2,0)
+		pygame.draw.circle(surface, pygame.Color('black'),(47,139),2,0)
+		sound.play()
 		for balls in note:
 			balls.render(surface)
+		for blueballs in note1:
+			blueballs.render(surface)
 	
 	def update(self):
 		for balls in note:
-			balls.update()
+			if balls.isCollide():
+				print balls.x , balls.x+balls.radius*2
+				if pygame.key.get_pressed()[K_SPACE]:
+					balls.delete()
+				print "collide"
 
+			balls.update()
+#			score.update(balls)
+		for blueballs in note1:
+			if blueballs.isCollide():
+				if pygame.key.get_pressed()[K_b]:
+					blueballs.delete()
+
+			blueballs.update()
 def main():
 	game = Memory()
 	game.run()
